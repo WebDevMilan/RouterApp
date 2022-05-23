b7c05613c8d794ebbab9fc2cb712bad8456abf1d
Jesus Milan <milan.jesusdev@gmail.com>
2022-05-22 20:58:06 -0400

    added react router dom and started to use some of its components

---
+++ src/components/navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    // Link prevents additional requests to the server results in better performance
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/products">Products</Link>
+      </li>
+      <li>
+        <Link to="/posts/2018/06">Posts</Link>
+      </li>
+      <li>
+        <Link to="/admin">Admin</Link>
+      </li>
+    </ul>
+  );
+};
+
+export default NavBar;
